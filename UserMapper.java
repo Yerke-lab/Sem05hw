e24733f46fb0301e3932ca21ff516cef6b2e816c
Yerke-lab <110925046+Yerke-lab@users.noreply.github.com>
2023-02-04 16:40:07 +0600

    Update UserMapper.java

--- UserMapper.java
+++ UserMapper.java
@@ -12,12 +12,12 @@ public class UserMapper {
     
 
     public User mapNew(String line){
-        String[] lines = line.split(";");
+        String[] lines = line.split("; ");
         return new User(lines[0], lines[1], lines[2], lines[3]);
     }
     
     public String mapNew(User user) {
-        return String.format("%s;%s;%s;%s", user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
+        return String.format("%s; %s; %s; %s", user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
     }
     
     public String mapOld(User user) {      
